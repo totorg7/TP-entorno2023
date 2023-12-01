2639f2240a56b3cf26f2933fc02bac30d53a073e
Luciana <luuluu135@gmail.com>
2023-12-01 20:44:43 -0300

    Arreglo de parte de uso de CPU, agregado de uso de RAM y espacio

--- monitor.sh
+++ monitor.sh
@@ -7,11 +7,17 @@ FILENAME="reporte"
 DATE=$(date +%F-%T)
 OUTPUT_FILE=$OUTPUT_DIR/$FILENAME-$DATE.txt
 PROCESOS=$(($(ps -aux | wc -l) -1))
-USO_CPU=$(ps -aux | grep -v USER | tr -s " " | cut -d " " -f 3 | sum | cut -d " " -f 1)
+USO_CPU=$(top -b -n 1 | grep '%Cpu(s)' | cut -d " " -f 3)
+USO_RAM=$(top -b -n 1 | grep 'MiB Mem')
+ESPACIO_DISC=$(df -T -h | grep 'ext4' | cut -d " " -f 21)
 
 
 echo "Usuario: $USER" > $OUTPUT_FILE
 echo "Uso de CPU: %$USO_CPU" >> $OUTPUT_FILE
 echo "Numero de proceso: $PROCESOS " >> $OUTPUT_FILE
-echo "Usuario: $USER" >> $OUTPUT_FILE
+echo "Uso de RAM: %$USO_RAM" >> $OUTPUT_FILE
+echo "Uso de disco duro: %$ESPACIO_DISC" >> $OUTPUT_FILE
+
+
+
 
