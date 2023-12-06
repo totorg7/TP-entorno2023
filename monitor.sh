c7ff204d742f79337f4b41fbd58e59ae7a044427
totorg7 <totorg7@gmail.com>
2023-12-06 17:53:14 -0300

    Correcciones al monitor

--- monitor.sh
+++ monitor.sh
@@ -9,7 +9,7 @@ OUTPUT_FILE=$OUTPUT_DIR/$FILENAME-$DATE.txt
 PROCESOS=$(($(ps -aux | wc -l) -1))
 USO_CPU=$(top -b -n 1 | grep '%Cpu(s)' | cut -d " " -f 3)
 USO_RAM=$(top -b -n 1 | grep 'MiB Mem')
-ESPACIO_DISC=$(df -T -h | grep 'ext4' | cut -d " " -f 21)
+ESPACIO_DISC=$(df -T -h | grep 'ext4' | tr -s " " | cut -d " " -f 5)
 
 
 echo "Usuario: $USER" > $OUTPUT_FILE
@@ -18,6 +18,4 @@ echo "Numero de proceso: $PROCESOS " >> $OUTPUT_FILE
 echo "Uso de RAM: %$USO_RAM" >> $OUTPUT_FILE
 echo "Uso de disco duro: %$ESPACIO_DISC" >> $OUTPUT_FILE
 
-
-
-
+cat "$(ls -t outputs/reporte-* | head -n 1)" 
